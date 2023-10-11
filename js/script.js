4cf227eafeb8151f47c56472b4e958cf05e6c414
L-sunsui <dna4570@gmail.com>
2023-10-11 16:30:29 +0900

    rollCommit
    rollCommit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// let dom = document.querySelector('#root')
+
+// let element = React.createElement(
+//   'div',{},[
+//     React.createElement(
+//       'h2',{},"Hello !"
+// ),
+// React.createElement(
+//   'h4',{className:'alert alert-primary'},"React sample page."
+// ),
+// React.createElement(
+//   'ul',{className:'list-group'}, [
+//     React.createElement(
+//       'li',{className:'list-group-item'},"First item."
+//     ),
+//     React.createElement(
+//       'li',{className:'list-group-item'},"Second item."
+//     ),
+//     React.createElement(
+//       'li',{className:'list-group-item'},"Third item."
+//     ),
+//   ]
+// ),
+// ])
+
+// ReactDOM.render(element,dom);
+// let dom
+// = document.querySelector('#root')
+
+// // let title = "新タイトル"
+// let message = "新しいメッセージです。"
+// let link = "http://google.com"
+
+// let el = (
+//   <div className="alert alert-primary">
+//     <h4>{message}</h4>
+//     <h5><a href={link}>this is link!</a></h5>
+//   </div>
+// )
+
+// ReactDOM.render(el.dom)
